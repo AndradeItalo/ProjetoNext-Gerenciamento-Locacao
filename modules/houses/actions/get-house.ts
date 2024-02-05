4bde14827727b7141a4b3d1bbf18add88a26ab92
Douglas de Carvalho Gemir <dcg@windows.cin.ufpe.br>
2024-02-05 14:51:10 -0300

    feat: getHouse created

--- modules/houses/actions/get-house.ts
+++ modules/houses/actions/get-house.ts
@@ -1,2 +1,18 @@
-export default function Page({ params }: { params: { slug: string } }) {
+import { db as prisma } from "@/lib/db";
+
+export const getHouse  = async (id:string) =>{
+  try {
+    const house = await prisma.immobile.findUnique({
+      where: {id:id},
+    });;
+
+    if (!house) {
+      return {error: "can't find this house!"};
+    }
+
+    return house;
+  } catch (error) {
+    return error;
+  }
 }
+
