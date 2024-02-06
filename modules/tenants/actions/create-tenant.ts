0a45e8beed977c919936b804b7765833cd253665
Italo Andrade <ifa2@cin.ufpe.br>
2024-02-05 22:28:09 -0300

    Refactor: Refactoring tenant CRUD to use ZOD

--- modules/tenants/actions/create-tenant.ts
+++ modules/tenants/actions/create-tenant.ts
@@ -1,9 +1,28 @@
 import { db as prisma } from "@/lib/db";
-import { Tenant } from "@prisma/client";
+import { createTenantSchema } from "@/schemas";
+import { z } from "zod";
 
-export const createTenant = async (data:Tenant) => {
+export const createTenant = async (
+    values: z.infer<typeof createTenantSchema>) => {
+
+    const validatedFields = createTenantSchema.safeParse(values);
+
+    if (!validatedFields.success) {
+        return { error: "Invalid fields!" };
+      }
+
+    const {cpf, email, name, number } =
+        validatedFields.data;
+
     try {
-        await prisma.tenant.create({data});
+        const newTenant = await prisma.tenant.create({
+            cpf, 
+            email, 
+            name, 
+            number
+        });
+
+        return newTenant;
     } catch (error) {
         console.log(error);
         return error;
